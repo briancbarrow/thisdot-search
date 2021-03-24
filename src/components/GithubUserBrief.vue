c83dfc51b7818dbb9772d079761a91bc672ba21f
Brian <briancbarrow@gmail.com>
2021-03-24 07:54:46 -0600

    added basic user brief component to make sure I was grabbing data correctly

---
+++ src/components/GithubUserBrief.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ userInfo.login }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    userInfo: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss"></style>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,32 +1,44 @@
 <template>
   <div>
     <h1>Search</h1>
-    <!-- <form @submit.prevent="callApi"> -->
-    <input v-model="searchTerm" placeholder="Search" />
+    <input v-model="searchTerm" placeholder="Search for Github Users" />
     <button @click="callApi">Submit</button>
-    <!-- </form> -->
+    <h2 v-if="totalUsers">{{ totalUsers }} Users</h2>
+    <GithubUserBrief
+      :key="user.id"
+      v-for="user in users.value"
+      :userInfo="user"
+    />
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
+import GithubUserBrief from './GithubUserBrief.vue';
 export default {
+  components: { GithubUserBrief },
   setup() {
     const url = 'https://api.github.com/search/users?q=';
     const options = {
       method: 'GET',
     };
     const searchTerm = ref('');
+    const users = reactive({});
+    const totalUsers = ref(null);
     const callApi = async () => {
       const apiResponse = await fetch(`${url}${searchTerm.value}`, options);
-      const body = await apiResponse.json();
-      console.log('body', body);
-      console.log('value', window.location);
+      const json = await apiResponse.json();
+      console.log('body', json.items);
+      users.value = json.items;
+      const numberFormat = new Intl.NumberFormat('en-US');
+      totalUsers.value = numberFormat.format(json.total_count);
     };
 
     return {
       searchTerm,
       callApi,
+      totalUsers,
+      users,
     };
   },
 };
