34a2d00ab271f016c98fc71f8d9f62667b415be7
Brian <briancbarrow@gmail.com>
2021-03-24 08:42:53 -0600

    Added tailwind styling to search input and button

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,10 +1,31 @@
 <template>
   <div>
     <h1>Search</h1>
-    <input v-model="searchTerm" placeholder="Search for Github Users" />
-    <button @click="callApi">Submit</button>
-    <h2 v-if="totalUsers">{{ totalUsers }} Users</h2>
-    <div class="max-w-md mx-auto py-12 px-4 sm:px-0 md:py-16">
+    <div class="w-full max-w-xs mx-auto">
+      <label for="searchTerm" class="sr-only">Search Users</label>
+      <input
+        type="text"
+        v-model="searchTerm"
+        name="searchTerm"
+        id="searchTerm"
+        class="py-2 px-3 shadow-sm focus:ring-green-500 focus:border-green-500 block w-full sm:text-sm border border-gray-500 rounded-md"
+        placeholder="Brian Barrow"
+      />
+      <button
+        type="button"
+        @click="callApi"
+        class="mt-2 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+      >
+        Submit
+      </button>
+    </div>
+    <h2
+      class="mt-4 text-lg leading-6 font-medium text-gray-900"
+      v-if="totalUsers"
+    >
+      {{ totalUsers }} Users
+    </h2>
+    <div class="max-w-md mx-auto pb-12 px-4 sm:px-0 md:pb-16">
       <ul class="divide-y divide-gray-200">
         <li :key="user.id" v-for="user in users.value" class="py-4 flex">
           <GithubUserBrief :userInfo="user" />
